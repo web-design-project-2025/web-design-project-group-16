26b86913031ef49844dc106edf39a9a9c2dc5c1f
velo24dl <velo24dl@student.ju.se>
2025-04-16 16:02:37 +0200

    Loading Data continue

--- JS/movieLoadData.js
+++ JS/movieLoadData.js
@@ -1,38 +1,33 @@
 const button = document.querySelector("#movieDetailsButton");
-const request = event.target;
-const response = request.response;
+const displayMovie = document.querySelector("#movieDetails");
+// const request = event.target;
+// const response = request.response;
 
 function finishedLoadingHandler(event) {
-    const request = event.target;
-    const response = request.response;
-    /*const displayMovie = document.querySelector("#movieDetails");*/
-    /*displayMovie.innerText = response[0];*/
-    console.log(response[0]);
+  const request = event.target;
+  const response = request.response;
+  console.log(response[0]);
 }
 
-
 function loadData() {
-    const request = new XMLHttpRequest();
-    request.responseType = "json";
+  const request = new XMLHttpRequest();
+  request.responseType = "json";
 
-    const url = "JSON/movie.json";
-    const method = "GET";
-    request.open(method, url);
-    request.addEventListener("load", finishedLoadingHandler);
-    request.send();
+  const url = "JSON/movie.json";
+  const method = "GET";
+  request.open(method, url);
+  request.addEventListener("load", finishedLoadingHandler);
+  request.send();
 }
 
 function loadHandler() {
- 
-    /*button.addEventListener("click", finishedLoadingHandler);*/
-    loadData();
-    
+  loadData();
 }
+
+function getMovieByID(id) {}
+
 window.addEventListener("load", loadHandler);
 
-/*button.addEventListener ("click", loadData); {
-    finishedLoadingHandler();
-    console.log(response[0]);
-};*/
-
-
+// button.addEventListener("click", function (event) {
+//   finishedLoadingHandler();
+// });
