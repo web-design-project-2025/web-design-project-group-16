ae529347c3fd5e8eca9466ae5e9dd3db2a62d7c2
majoh05 <joma24zx@student.ju.se>
2025-04-16 15:17:44 +0200

    Lading data continued

--- JS/movieLoadData.js
+++ JS/movieLoadData.js
@@ -1,8 +1,15 @@
+const button = document.querySelector("#movieDetailsButton");
+const request = event.target;
+const response = request.response;
+
 function finishedLoadingHandler(event) {
     const request = event.target;
     const response = request.response;
-    console.log(response[1]);
+    /*const displayMovie = document.querySelector("#movieDetails");*/
+    /*displayMovie.innerText = response[0];*/
+    console.log(response[0]);
 }
+
 
 function loadData() {
     const request = new XMLHttpRequest();
@@ -16,6 +23,16 @@ function loadData() {
 }
 
 function loadHandler() {
+ 
+    /*button.addEventListener("click", finishedLoadingHandler);*/
     loadData();
+    
 }
 window.addEventListener("load", loadHandler);
+
+/*button.addEventListener ("click", loadData); {
+    finishedLoadingHandler();
+    console.log(response[0]);
+};*/
+
+
