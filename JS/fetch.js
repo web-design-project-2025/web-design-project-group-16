f81507771fad6287dfbfd2490688ea192f34f821
velo24dl <velo24dl@student.ju.se>
2025-04-27 16:34:43 +0200

    Working fetching Data form external API to console log

--- JS/fetch.js
+++ JS/fetch.js
@@ -1,16 +1,16 @@
 let movies = [];
 let reviews = [];
 const contentElement = document.getElementById("content");
-async function loadData() {
-    const movieResponse = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=f20a19d8&"); //Figure out how to fetch the "correct" movie id, arrays
-    const movieData = await movieResponse.json();
-    // movies = movieData.movie;
-    movies = movieData.r;
-    console.log(movieData);
-    console.log(movies);
-    console.log(movieResponse);
 
-    renderContent();
+async function loadData() {
+  const movieResponse = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=41633bc6f1e4947d357fb72eeb8115ed" // How to implement the key into the js taken form:https://www.youtube.com/watch?v=03FAepR-WVQ
+  ); //Figure out how to fetch the "correct" movie id, arrays
+  const movieData = await movieResponse.json();
+  movies = movieData.movie;
+  console.log(movieData);
+
+  renderContent();
 }
 
 /*async function loadData() {
@@ -28,32 +28,32 @@ async function loadData() {
 }*/
 
 function getMovieById(id) {
-    return movies.find((movie) => movie.id === id);
+  return movies.find((movie) => movie.id === id);
 }
 
 function createReviewElement(review, movie) {
-    const reviewElement = document.createElement("article");
-    reviewElement.classList.add("review");
+  const reviewElement = document.createElement("article");
+  reviewElement.classList.add("review");
 
-    const headerElement = document.createElement("div");
-    headerElement.classList.add("padding", "header");
-    reviewElement.appendChild(headerElement);
+  const headerElement = document.createElement("div");
+  headerElement.classList.add("padding", "header");
+  reviewElement.appendChild(headerElement);
 
-    const imageElement = document.createElement("img");
-    imageElement.src = review.image;
-    reviewElement.appendChild(imageElement);
+  const imageElement = document.createElement("img");
+  imageElement.src = review.image;
+  reviewElement.appendChild(imageElement);
 
-    return reviewElement;
+  return reviewElement;
 }
 
 function renderContent() {
-    contentElement.innerHTML = "";
+  contentElement.innerHTML = "";
 
-    for (let review of reviews) {
-        const movie = getMovieById(review.movie_id);
-        const reviewElement = createReviewElement(review, movie);
-        contentElement.appendChild(reviewElement);
-    }
+  for (let review of reviews) {
+    const movie = getMovieById(review.movie_id);
+    const reviewElement = createReviewElement(review, movie);
+    contentElement.appendChild(reviewElement);
+  }
 }
 
 loadData();
