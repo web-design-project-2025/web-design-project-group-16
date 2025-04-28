f768b820b2b166bf1b1f461fbad8de69156aa4c9
velo24dl <velo24dl@student.ju.se>
2025-04-28 11:55:49 +0200

    Info from API showing up on the site, including correct posters

--- JS/fetch.js
+++ JS/fetch.js
@@ -4,56 +4,58 @@ const contentElement = document.getElementById("content");
 
 async function loadData() {
   const movieResponse = await fetch(
-    "https://api.themoviedb.org/3/movie/upcoming?api_key=41633bc6f1e4947d357fb72eeb8115ed" // How to implement the key into the js taken form:https://www.youtube.com/watch?v=03FAepR-WVQ
+    "https://api.themoviedb.org/3/discover/movie?api_key=41633bc6f1e4947d357fb72eeb8115ed" // How to implement the key into the js taken form:https://www.youtube.com/watch?v=03FAepR-WVQ
   ); //Figure out how to fetch the "correct" movie id, arrays
   const movieData = await movieResponse.json();
-  movies = movieData.movie;
+  movies = movieData.results;
   console.log(movieData);
 
   renderContent();
 }
 
-/*async function loadData() {
-    const movieResponse = await fetch("http://www.omdbapi.com/?apikey=[yourkey]&");
-    const movieJSON = await movieResponse.json();
-    movies = movieJSON.movie;
-    console.log(movieJSON);
-
-    const reviewResponse = await fetch("JSON/user.json");
-    const reviewJSON = await reviewResponse.json();
-    reviews = reviewJSON.review;
-    console.log(reviewJSON);
-
-    renderContent();
-}*/
-
 function getMovieById(id) {
   return movies.find((movie) => movie.id === id);
 }
 
-function createReviewElement(review, movie) {
-  const reviewElement = document.createElement("article");
-  reviewElement.classList.add("review");
+function displayMovieElement(movie) {
+  const movieElement = document.createElement("div");
+  movieElement.classList.add("movie-container");
 
-  const headerElement = document.createElement("div");
-  headerElement.classList.add("padding", "header");
-  reviewElement.appendChild(headerElement);
+  // POSTER ELEMENT
+  const posterElement = document.createElement("img");
+  posterElement.classList.add("movie-poster");
+  //console.log(movie);
+  posterElement.src = "https://image.tmdb.org/t/p/w1280/" + movie.poster_path;
+  movieElement.appendChild(posterElement);
 
-  const imageElement = document.createElement("img");
-  imageElement.src = review.image;
-  reviewElement.appendChild(imageElement);
+  // TITLE ELEMENT
+  const titleElement = document.createElement("h1");
+  titleElement.classList.add("title");
+  //console.log(movie);
+  titleElement.textContent = movie.original_title;
+  movieElement.appendChild(titleElement);
 
-  return reviewElement;
+  // RELEASE DATE
+  //   const releaseDateElement = document.createElement("h2");
+  //   releaseDateElement.classList.add("release-date");
+  //   //console.log(movie);
+  //   releaseDateElement.textContent = movie.release_date;
+  //   movieElement.appendChild(releaseDateElement);
+
+  return movieElement;
 }
 
 function renderContent() {
   contentElement.innerHTML = "";
 
-  for (let review of reviews) {
-    const movie = getMovieById(review.movie_id);
-    const reviewElement = createReviewElement(review, movie);
-    contentElement.appendChild(reviewElement);
+  for (let movie of movies) {
+    //const film = getMovieById(movie.movie_id);
+    const descriptionElement = displayMovieElement(movie);
+    contentElement.appendChild(descriptionElement);
   }
+  //const movie = getMovieById(1197306);
+  //const descriptionElement = displayMovieElement(movie);
+  //contentElement.appendChild(descriptionElement);
 }
 
 loadData();
